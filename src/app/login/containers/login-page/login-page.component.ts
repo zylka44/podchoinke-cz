7009e033c42bc0fe8819bb4c2ffdd8ecd3cd3026
zylka44 <zancza@gmail.com>
2020-10-24 21:01:13 +0200

    Set login-user routing

--- src/app/login/containers/login-page/login-page.component.ts
+++ src/app/login/containers/login-page/login-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onButtonClick() {
+    this.router.navigate([`/user`]);
   }
-
 }
--- src/app/user-page/containers/user-page/user-page.component.ts
+++ src/app/user-page/containers/user-page/user-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.scss']
+  styleUrls: ['./user-page.component.scss'],
 })
 export class UserPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogOut() {
+    this.router.navigate([`/login`]);
   }
-
 }
