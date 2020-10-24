59971145ab315e4879d355154929419e0061ed1d
zylka44 <zancza@gmail.com>
2020-10-24 18:53:52 +0200

    Init production firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDB0ptfZUF7Zsw94-8QqPxZx4mNRcGO0HI',
+    authDomain: 'podchoinke-prod.firebaseapp.com',
+    databaseURL: 'https://podchoinke-prod.firebaseio.com',
+    projectId: 'podchoinke-prod',
+    storageBucket: 'podchoinke-prod.appspot.com',
+    messagingSenderId: '919696429552',
+    appId: '1:919696429552:web:736a00fd8077f8a6ac0d35',
+  },
 };
