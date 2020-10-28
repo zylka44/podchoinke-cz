9c3847667fa37130391508978b78996d96fb2eaa
zylka44 <zancza@gmail.com>
2020-10-28 20:08:33 +0100

    Set login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,4 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  itemsRef: AngularFireList<any>;
-  items: Observable<any[]>;
-
-  constructor(db: AngularFireDatabase) {
-    this.itemsRef = db.list('persons');
-    this.items = this.itemsRef
-      .snapshotChanges()
-      .pipe(
-        map((changes) =>
-          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
-        )
-      );
-    this.items.subscribe(console.log);
-  }
-
-  addItem(newName: string) {
-    this.itemsRef.push({ text: newName });
-  }
-  updateItem(key: string, newText: string) {
-    this.itemsRef.update(key, { text: newText });
-  }
-  deleteItem(key: string) {
-    console.log(key);
-    this.itemsRef.remove(key);
-  }
-  deleteEverything() {
-    this.itemsRef.remove();
-  }
-}
+export class AppComponent {}
--- src/app/login/containers/login-page/login-page.component.ts
+++ src/app/login/containers/login-page/login-page.component.ts
@@ -1,19 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
-  users = ['tata', 'mama', 'mariusz', 'zaneta', 'marek', 'damian', 'maciek'];
+export class LoginPageComponent {
+  usersRef: AngularFireList<any>;
+  users: Observable<any[]>;
+  selectedUserName: string;
+  selectedUserExpectedPassword: string;
+  selectedUserEnterdPassword: string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private db: AngularFireDatabase) {
+    this.usersRef = db.list('users');
+    this.users = this.usersRef.valueChanges();
 
-  ngOnInit(): void {}
+    document.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter' && this.selectedUserEnterdPassword) {
+        this.onLogin();
+      }
+    });
+  }
 
-  onButtonClick(name: string): void {
-    this.router.navigate([`/user/${name}`]);
+  onButtonClick(name: string, password: string): void {
+    this.selectedUserName = name;
+    this.selectedUserExpectedPassword = password;
+    setTimeout(() => document.getElementById('password').focus(), 0);
+  }
+
+  onLogin(): void {
+    console.log(this.selectedUserEnterdPassword);
+    console.log(this.selectedUserExpectedPassword);
+    if (this.selectedUserEnterdPassword === this.selectedUserExpectedPassword) {
+      document.removeEventListener('keypress', () => {});
+      this.router.navigate([`/user/${this.selectedUserName}`]);
+    } else {
+      document.getElementById('loginError').innerHTML = 'nieprawidłowe hasło';
+    }
+  }
+
+  onInputChange(password: string): void {
+    this.selectedUserEnterdPassword = password;
+    document.getElementById('loginError').innerHTML = '';
   }
 }
