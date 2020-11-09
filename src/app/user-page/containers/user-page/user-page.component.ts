aeae11a9085844a2ca931b8056c9fb11c0854d42
zylka44 <zancza@gmail.com>
2020-11-09 17:43:28 +0100

    Set onOwnerButtonClick

--- src/app/user-page/containers/user-page/user-page.component.ts
+++ src/app/user-page/containers/user-page/user-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from 'src/app/models/users';
@@ -14,28 +14,31 @@ export class UserPageComponent implements OnInit {
   users$: Observable<User[]>;
   name: string;
   user$: Observable<User>;
+  letterOwner$: Observable<User>;
   userFullName$: Observable<string>;
-  userKey$: Observable<string>;
   otherUsers$: Observable<User[]>;
 
   constructor(private router: Router, private usersService: UsersService) {}
-
-  onLogOut(): void {
-    this.router.navigate([`/login`]);
-  }
-
-  onOwnerButtonClick(ownerKey: string): void {
-    console.log(ownerKey);
-  }
 
   ngOnInit(): void {
     this.name = this.router.url.split('/')[2];
     this.user$ = this.usersService.getUserOfName(this.name);
     this.users$ = this.usersService.getUsers();
     this.userFullName$ = this.user$.pipe(map((user) => user.fullName));
-    this.userKey$ = this.user$.pipe(map((user) => user.key));
     this.otherUsers$ = this.users$.pipe(
       map((users) => users.filter((user) => user.name !== this.name))
     );
+    this.letterOwner$ = this.user$;
+  }
+
+  onLogOut(): void {
+    this.router.navigate([`/login`]);
+  }
+
+  onOwnerButtonClick(userKey: string): void {
+    this.letterOwner$ =
+      userKey === 'currentUser'
+        ? this.usersService.getUserOfName(this.name)
+        : this.usersService.getUserOfKey(userKey);
   }
 }
