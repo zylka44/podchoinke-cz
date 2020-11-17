05793ad0e6ac3a199037a56c0824bfc87b34f3f3
zylka44 <zancza@gmail.com>
2020-11-17 19:32:40 +0100

    Change font size

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyD7El4PyctsoYtlG9OFn1trQ8l1NBPLwCA',
-    authDomain: 'podchoinke-cz.firebaseapp.com',
-    databaseURL: 'https://podchoinke-cz.firebaseio.com',
-    projectId: 'podchoinke-cz',
-    storageBucket: 'podchoinke-cz.appspot.com',
-    messagingSenderId: '269325674549',
-    appId: '1:269325674549:web:993bf01f6aeb145110a97c',
+    apiKey: 'AIzaSyDB0ptfZUF7Zsw94-8QqPxZx4mNRcGO0HI',
+    authDomain: 'podchoinke-prod.firebaseapp.com',
+    databaseURL: 'https://podchoinke-prod.firebaseio.com',
+    projectId: 'podchoinke-prod',
+    storageBucket: 'podchoinke-prod.appspot.com',
+    messagingSenderId: '919696429552',
+    appId: '1:919696429552:web:736a00fd8077f8a6ac0d35',
   },
 };
